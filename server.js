7f47663fd2c675530a9d09fbab28df0d592ed029
Roman Loboda <hyawey2003@gmail.com>
2024-05-05 15:25:17 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const path = require('path');
 
 app.use(express.json());
 
-// Root route
-app.get('/', (req, res) => {
-    res.send('Welcome to the homepage');
-});
-
-// Use userController to handle user-related endpoints
-app.use('/signups', signups);
-
 // Production script 
 
 const staticFilesPath = path.join(__dirname, 'client', 'build');
@@ -21,6 +13,10 @@ app.use(express.static(staticFilesPath));
 app.get('*', (req, res) => {
     res.sendFile(path.join(staticFilesPath, 'index.html'));
 });
+
+// Use userController to handle user-related endpoints
+app.use('/signups', signups);
+
 
 // Start the server
 app.listen(8080, () => {

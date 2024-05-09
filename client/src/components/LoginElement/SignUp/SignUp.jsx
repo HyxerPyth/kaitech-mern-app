f37e573c20e0dc4a8a387e3e859eb7975faf3103
Roman Loboda <hyawey2003@gmail.com>
2024-05-09 16:14:25 -0400

    Added Donate Button

--- client/src/components/LoginElement/SignUp/SignUp.jsx
+++ client/src/components/LoginElement/SignUp/SignUp.jsx
@@ -48,7 +48,7 @@ const SignUpForm = (props) => {
 
     return (
 
-        <Form 
+        <Form className={style.formBack}
             onSubmit={onSubmit}> 
             {props => (
                     <form className={style.form} autoComplete="off" onSubmit={props.handleSubmit}>
@@ -174,7 +174,21 @@ const SignUpForm = (props) => {
 const SignUp = (props) => {
 
     return (
-        <SignUpForm createUser={props.createUser}/>
+        <div>
+            <div className={style.donateButton}>
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                    <input type="hidden" name="business" value="A3PQEFVJU37W4" />
+                    <input type="hidden" name="no_recurring" value="0" />
+                    <input type="hidden" name="item_name" value="Building a startup from scratch. Need help covering API costs to bring our vision to life." />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+            </div>
+
+            <SignUpForm createUser={props.createUser}/>
+        </div>
+
     );
 }
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,10 +7,20 @@ import style from './Home.module.css';
 const Home = (props) => {
     return (
         <div>
-            <div>
+            <div className={style.Text}>
                 <h2 className={style.Title}>KaiTech</h2>
                 <p className={style.Description}>you can use the signup button to register you phone number</p>
                 <p className={style.Description}>once you signed up, you will be able to call Andrew Tate</p>
+            </div>
+            <div className={style.donateButton}> 
+                <form action="https://www.paypal.com/donate" method="post" target="_top">
+                    <input type="hidden" name="business" value="A3PQEFVJU37W4" />
+                    <input type="hidden" name="no_recurring" value="0" />
+                    <input type="hidden" name="item_name" value="Building a startup from scratch. Need help covering API costs to bring our vision to life." />
+                    <input type="hidden" name="currency_code" value="USD" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
             </div>
            <div className={style.Container}>
                 <NavLink to="/signin">
